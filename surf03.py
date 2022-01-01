18bd4b6cae632873a15b9bc876a8cfc581ed67e6
Fernando Masanori <fmasanori@gmail.com>
2022-01-01 18:55:23 -0300

    Add files via upload

--- surf03.py
+++ surf03.py
@@ -4,7 +4,7 @@ for linha in f:
     nome, pontos = linha.split()
     notas.append(float(pontos))
 f.close()
-notas.sort()
-
-print (notas[:3])
+print (notas[0])
+print (notas[1])
+print (notas[2])
 
